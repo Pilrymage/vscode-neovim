a56f7633e0145da5e52393419e2155d03eee46b8
Kear Coara <156th@pilrymage.xyz>
2023-01-10 11:34:42 +0800

    大写字母映射

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ map("n","i","l",opts)
 map("n","h","e",opts)
 map("n","j","u",opts)
 map("n","k","n",opts)
-map("n","l","e",opts)
+map("n","l","i",opts)
 map("v","u","k",opts)
 map("v","n","h",opts)
 map("v","e","j",opts)
@@ -26,4 +26,16 @@ map("v","h","l",opts)
 map("v","i","e",opts)
 map("v","j","u",opts)
 map("v","k","n",opts)
-map("v","l","e",opts)
+map("v","l","i",opts)
+-- 带shift的按键也有相应修改。
+-- 方向键做了一点强化，不需要逆天的$和0了。
+map("n","N","0",opts)
+map("n","I","$",opts)
+map("n","U","5k",opts)
+map("n","E","5j",opts)
+
+-- 方向键
+-- 快捷键映射
+-- 即使用caps lock顶替esc,对左手小拇指而言还是很恼火。
+-- colemak中排的字母使用频率太高，退而求其次使用nk映射到esc.
+map("i","nk","<Esc>",opts)

f2ad7dfb0bb78a4217832464d23045813fb611d7
Kear Coara <156th@pilrymage.xyz>
2023-01-10 10:44:24 +0800

    更新方向键映射

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- neovim 配置
+-- 按个人习惯逐渐改进的配置方案，可视情况注释不需要的部分
+
+local map = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
+-- 最基础的修改：光标的方向键
+-- hjkl四个键得改掉后三个，索性全部换掉，因为hjkl的排布还是有点逆天
+--             u
+-- 现布局：   n e i
+-- h：光标到下一个词的开头，等同于e
+-- j：撤回，等同于u
+-- k：下一个搜索结果，等同于n
+-- l：插入，等同于i
+map("n","u","k",opts)
+map("n","n","h",opts)
+map("n","e","k",opts)
+map("n","i","l",opts)
+map("n","i","e",opts)
+map("n","j","u",opts)
+map("n","k","n",opts)
+map("n","l","e",opts)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# vscode-neovim（Colemak键位）
+
+如名称所示，这个库用于保存vscode-neovim插件的配置。因为vscode与neovim和Colemak键盘互相有冲突，不可避免的要大改。
+
+个人使用Lua编写脚本。如果是连一个nvim都装不了的电脑，我大概不会想用它来码代码。
